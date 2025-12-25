620d7993a13ec5123ffa2ab0bfcd03beb9704b26
Alexander <alexander891622gmail.com>
2025-12-25 14:45:47 -0800

    Improved project structure

---
+++ Quicknote/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using YourApp.Views;
+
+namespace Quicknote;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+	}
+
+	protected override Window CreateWindow(IActivationState? activationState)
+	{
+		return new Window(new MainView());
+	}
+}
---
+++ Quicknote/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Quicknote;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

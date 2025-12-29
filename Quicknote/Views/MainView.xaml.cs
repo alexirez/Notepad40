9109d895209ec143d2ce59cadfe13810a618b29a
Alexander <alexander891622gmail.com>
2025-12-28 23:14:16 -0800

    Update MainView and include data bindings to link the view to MainViewModel

--- Quicknote/Views/MainView.xaml.cs
+++ Quicknote/Views/MainView.xaml.cs
@@ -1,9 +1,15 @@
-namespace YourApp.Views;
+using System.Text.Json;
+using Quicknote.Models;
+using Quicknote.Services;
+using Quicknote.ViewModels;
+
+namespace Quicknote.Views;
 
 public partial class MainView : ContentPage
 {
     public MainView()
     {
         InitializeComponent();
+        BindingContext = new MainViewModel(new FileSaver());
     }
 }
